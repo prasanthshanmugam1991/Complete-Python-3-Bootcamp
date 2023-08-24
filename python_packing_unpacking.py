c2a2b47f0927e5d5a634875e555c32e0a24bf329
prasanthshanmugam1991 <48430779+prasanthshanmugam1991@users.noreply.github.com>
2023-08-24 16:07:49 +0530

    Create python_packing_unpacking.py

---
+++ python_packing_unpacking.py
@@ -0,0 +1,19 @@
+t = [12345, 54321, 'hello!']
+x, y, z = t
+print(x, y,z)
+# 12345 54321 hello!
+
+t = (12345, 54321, 'hello!')
+x, y, z = t
+print(x, y,z)
+# 12345 54321 hello!
+
+t = {12345, 54321, 'hello!'}
+x, y, z = t
+print(x, y,z)
+# 12345 54321 hello!
+
+t = "abc"
+x, y, z = t
+print(x, y, z)
+# a b c
